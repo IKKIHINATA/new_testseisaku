2ddd62f0d3bf3869ac4858595afdf8e513106ccd
IKKIHINATA <hinata_i@tokium.jp>
2025-08-12 18:34:24 +0900

    vite.config.tsにoptimizeDepsを追加してFirebaseの解決エラーを修正

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // FirebaseのモジュールをViteが正しく見つけられるように、
-  // この optimizeDeps の設定を追加しました。
+  // ↓↓↓ この optimizeDeps の設定を追記してください ↓↓↓
   optimizeDeps: {
     include: ['firebase/app', 'firebase/firestore'],
   },
